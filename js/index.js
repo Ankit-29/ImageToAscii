7f21344f9ff98bd8b0e8906da9297dcf3736c7d3
Ankit-29 <iamankitkumar29@gmail.com>
2020-02-14 23:25:19 +0530

    Gray-Scale

--- js/index.js
+++ js/index.js
@@ -8,13 +8,13 @@ imageToAscii = () => {
     let img = new Image();
     img.src = URL.createObjectURL(imgInp.files[0]);
     img.onload = function () {
-        ctx.filter = 'grayscale(100%)';
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
         let ImageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         for (let i = 0; i < ImageData.data.length; i += 4) {
-            // ImageData.data[i] = 0;
-            // ImageData.data[i + 1] = 0;
-            // ImageData.data[i + 2] = 0;
+            let avg = ImageData.data[i]+ImageData.data[i + 1]+ImageData.data[i + 2];
+            ImageData.data[i] = avg;
+            ImageData.data[i + 1] = avg;
+            ImageData.data[i + 2] = avg;
 
         }
         ctx.putImageData(ImageData, 0, 0);
